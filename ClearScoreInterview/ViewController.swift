66df6b158593989a3de418a2437f705b0b40c5d7
Jérémy Touzy <jtouzy2@gmail.com>
2020-02-21 13:39:32 +0100

    init: base project structure

---
+++ ClearScoreInterview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearScoreInterview
+//
+//  Created by Jérémy TOUZY on 21/02/2020.
+//  Copyright © 2020 jtouzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
